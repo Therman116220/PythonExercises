b94d710dee7944a048ba81c4a8388c4510f63bf9
Therman116220 <thermanhaynes@gmail.com>
2022-10-22 01:26:44 -0500

    had some fun with BeautifulSoup grabbing data

---
+++ Conversions/BeautifulSoup.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as toolOfChoice
+
+print("Input youtube extension")
+extension = input("")
+print("Input element such as link,body,img & footer")
+element = input("")
+url = 'https://youtube.com/' + extension
+req = requests.get(url)
+beautsoop = toolOfChoice(req.content, 'html.parser')
+
+'''Find from within the html document (this tag)'''
+data = beautsoop.find(element)
+print(data)
