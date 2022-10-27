e6d63893107b930aee6c5cdee01afd76e17eaf12
Therman116220 <thermanhaynes@gmail.com>
2022-10-27 09:15:05 -0500

    Lists

--- Conversions/Lists.py
+++ Conversions/Lists.py
@@ -1,7 +1,7 @@
 import random
 
 lunchMenu = ['Pop Tart', 'Croissant', 'Chicken Soup', 'PB & J']
-'''dinnerMenu = ['Lasagna', 'Hawaiian Pizza', 'Tacos', 'Chicken & Rice']'''
+'''dinnerMenu = ['Lasagna', 'Hawaiian Pizza', 'Tacos', 'Chicken & Rice' ]'''
 print('Menu: ', lunchMenu)
 
 random_item = random.choice(lunchMenu)
