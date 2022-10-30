d2c8cc47976f2b548f7343f48cd6c8e705fb47a2
Therman116220 <thermanhaynes@gmail.com>
2022-10-29 23:41:24 -0500

    queue practice in python

---
+++ Conversions/DS & A/Queue.py
@@ -0,0 +1,14 @@
+from  queue import Queue
+
+
+
+newqueue = Queue()
+
+
+newqueue.put(1)
+'''queueName.get()'''
+
+''' To utilize queueName i'm making it into a list '''
+print(list(newqueue.queue))
+
+
