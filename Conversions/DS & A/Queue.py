3c4831d3b86b5a05f5d5d043d3d95910748f7628
Therman116220 <thermanhaynes@gmail.com>
2022-10-29 23:59:33 -0500

    more queue practice in python

--- Conversions/DS & A/Queue.py
+++ Conversions/DS & A/Queue.py
@@ -1,14 +1,30 @@
+import queue
 from  queue import Queue
 
+q = queue.Queue()
+numqueue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in numqueue:
+    q.put(i)
+    print("Numbers: ", i)
 
 
-newqueue = Queue()
+
+newnamesqueue = Queue()
 
 
-newqueue.put(1)
+newnamesqueue.put("Shell")
+newnamesqueue.put("Chevron")
+newnamesqueue.put("Buckeyes")
+newnamesqueue.put("Loves")
+newnamesqueue.put("QT")
+newnamesqueue.put("Seven-Eleven")
+newnamesqueue.put("Circle K")
+newnamesqueue.put("Valero")
+newnamesqueue.put("Exxon")
+
 '''queueName.get()'''
 
 ''' To utilize queueName i'm making it into a list '''
-print(list(newqueue.queue))
+print(list(newnamesqueue.queue))
 
 
