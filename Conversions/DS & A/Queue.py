fd6c738f55fb28b113902d90835c2ba5ffdfc750
Therman116220 <thermanhaynes@gmail.com>
2022-10-30 00:23:26 -0500

    practice

--- Conversions/DS & A/Queue.py
+++ Conversions/DS & A/Queue.py
@@ -1,30 +1,37 @@
 import queue
 from  queue import Queue
+'''First In First Out (FIFO)'''
+
 
 q = queue.Queue()
+
 numqueue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for i in numqueue:
-    q.put(i)
-    print("Numbers: ", i)
+i = 0
+for i in range(len(numqueue)):
+
+    if i == i:
+     q.put(i)
+    print(numqueue)
+    print("List #: ", i)
 
 
+fuelstop_queue = Queue()
 
-newnamesqueue = Queue()
 
-
-newnamesqueue.put("Shell")
-newnamesqueue.put("Chevron")
-newnamesqueue.put("Buckeyes")
-newnamesqueue.put("Loves")
-newnamesqueue.put("QT")
-newnamesqueue.put("Seven-Eleven")
-newnamesqueue.put("Circle K")
-newnamesqueue.put("Valero")
-newnamesqueue.put("Exxon")
+fuelstop_queue.put("Shell")
+fuelstop_queue.put("Chevron")
+fuelstop_queue.put("Buckeyes")
+fuelstop_queue.put("Loves")
+fuelstop_queue.put("QT")
+fuelstop_queue.put("Seven-Eleven")
+fuelstop_queue.put("Circle K")
+fuelstop_queue.put("Valero")
+fuelstop_queue.put("Exxon")
 
 '''queueName.get()'''
 
 ''' To utilize queueName i'm making it into a list '''
-print(list(newnamesqueue.queue))
+listed_queue = list(fuelstop_queue.queue)
+print(listed_queue)
 
 
