ba69bd430fdd9888881eb62b9146d20338eecc48
Therman116220 <thermanhaynes@gmail.com>
2022-11-10 23:36:46 -0600

    update

---
+++ Conversions/Basics/OOPS.py
@@ -0,0 +1,15 @@
+class Food (object):
+    def __int__(self, f, l, a):
+        self.firstname = f
+        self.lastName = l
+        self.age = a
+
+    def getFirstName(self):
+        return self.firstname
+    def getLastName(self):
+        return self.lastName
+    def getAge(self):
+        return self.age
+
+
+
