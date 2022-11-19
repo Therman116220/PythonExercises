8a79c054356c3d1c7af1917ab922b219de1fb5cd
Therman116220 <thermanhaynes@gmail.com>
2022-11-19 00:00:40 -0600

    Dictionary Of Lists

--- Conversions/Lists.py
+++ Conversions/Lists.py
@@ -6,3 +6,24 @@ print('Menu: ', lunchMenu)
 
 random_item = random.choice(lunchMenu)
 print('Breakfast: ' + str(random_item))
+
+print('-----------------------------------------')
+
+
+'''Dictionary of Lists'''
+
+daily_menu = {
+    'Breakfast': ['Pop Tart', 'Croissant', 'Chicken Soup', 'PB & J'],
+    'Lunch': ['Salad', 'Pizza', 'Sandwich'],
+    'Dinner': ['Chicken & Rice', 'Pizza', 'Sandwich']
+}
+
+
+print('Breakfast:\t', daily_menu['Breakfast'])
+print('Lunch:\t', daily_menu['Lunch'])
+print('Dinner:\t', daily_menu['Dinner'])
+
+
+
+
+
