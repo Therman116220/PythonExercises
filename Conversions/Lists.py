da01beed78cc292585fc3d811e8ddca422de203e
Therman116220 <thermanhaynes@gmail.com>
2022-11-19 00:12:37 -0600

    Dictionary Of Lists

--- Conversions/Lists.py
+++ Conversions/Lists.py
@@ -1,5 +1,7 @@
 import random
 
+
+print('Randomizer:')
 lunchMenu = ['Pop Tart', 'Croissant', 'Chicken Soup', 'PB & J']
 '''dinnerMenu = ['Lasagna', 'Hawaiian Pizza', 'Tacos', 'Chicken & Rice' ]'''
 print('Menu: ', lunchMenu)
@@ -13,16 +15,22 @@ print('-----------------------------------------')
 '''Dictionary of Lists'''
 
 daily_menu = {
-    'Breakfast': ['Pop Tart', 'Croissant', 'Chicken Soup', 'PB & J'],
-    'Lunch': ['Salad', 'Pizza', 'Sandwich'],
-    'Dinner': ['Chicken & Rice', 'Pizza', 'Sandwich']
+    'Breakfast:': ['Pop Tart', 'Croissant', 'Chicken Soup', 'PB & J'],
+    'Lunch:': ['Salad', 'Pizza', 'Sandwich'],
+    'Dinner:\n': ['Chicken & Rice', 'Pizza', 'Sandwich']
 }
-
-
-print('Breakfast:\t', daily_menu['Breakfast'])
-print('Lunch:\t', daily_menu['Lunch'])
-print('Dinner:\t', daily_menu['Dinner'])
-
+'''print('Print Keys but not values')
+for item in daily_menu:
+    print(item)'''
+for name, daily_menu in daily_menu.items():
+    print(name)
+    for item in daily_menu:
+        print('\t' + item)
+    print('-----------------------------------------')
+'''print('Breakfast:', daily_menu['Breakfast'])
+print('Lunch:', daily_menu['Lunch'])
+print('Dinner:', daily_menu['Dinner'])
+'''
 
 
 
