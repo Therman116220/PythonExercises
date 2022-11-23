ec12448d69f084cdcf1cf0bc2c8ac7e639c73415
Therman116220 <thermanhaynes@gmail.com>
2022-11-22 18:52:04 -0600

    update

---
+++ Conversions/ExpensesCalculator.py
@@ -0,0 +1,11 @@
+
+
+expenses = [10.50, 11, 6, 13, 14, 4, 16]
+sum = 0
+
+for i in expenses:
+    sum = sum + i
+    print("Spent this week:" + sum)
+
+
+<!-- Popups v1.9.3.8 - https://wordpress.org/plugins/popups/ -->
