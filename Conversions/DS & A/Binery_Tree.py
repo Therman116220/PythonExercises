cf4a6e500235150afc73db929d2ea14d813ee814
Therman116220 <thermanhaynes@gmail.com>
2022-11-28 21:50:30 -0600

    binary_tree

--- Conversions/DS & A/Binery_Tree.py
+++ Conversions/DS & A/Binery_Tree.py
@@ -1,7 +1,48 @@
 
-
 class Node:
-    def __init__(self, value):
+
+ def __init__(self, value):
+
         self.data = value
         self.left = None
         self.right = None
+
+def bfs(root_node):
+     queue = []
+     if root_node:
+          queue.append(root_node)
+     while queue:
+         cur_node = queue.pop(0)
+         print(cur_node.data, end=" ")
+         if cur_node.left:
+             queue.append(cur_node.left)
+         if cur_node.right:
+             queue.append(cur_node.right)
+
+
+if __name__ == '__main__':
+     root = Node(1)
+     root.left = Node(2)
+     root.right = Node(3)
+     root.left.left = Node(4)
+     root.left.right = Node(5)
+     root.right.left = Node(6)
+     root.right.right = Node(7)
+     '''print(root)'''
+     bfs(root)
+
+
+     print(root.left)
+     print(root.right)
+     print(root.left.left)
+     print(root.left.right)
+     print(root.right.left)
+     print(root.right.right)
+     print(root.left.left.left)
+     print(root.left.left.right)
+     print(root.left.right.left)
+     print(root.left.right.right)
+
+
+
+
--- Conversions/DS & A/Fibonacci.py
+++ Conversions/DS & A/Fibonacci.py
@@ -1,3 +1,8 @@
+'''Drill One'''
+
+'''* Yield Utility Statement : https://www.simplilearn.com/tutorials/python-tutorial/yield-in-python'''
+
+
 def fibonacci():
     x = 1
     y = 1
@@ -5,10 +10,8 @@ def fibonacci():
         yield x
         x, y = y, x + y
 
+
 for i, num in enumerate(fibonacci()):
     print(f"%d" % num)
     if i == 15:
         break
-
-   
-    
